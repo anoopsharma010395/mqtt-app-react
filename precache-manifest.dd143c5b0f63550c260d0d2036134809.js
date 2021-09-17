d58ebacedf11390caa85edce003a4201708e6cc2
Anoop Sharma <38686430+anoopsharma010395@users.noreply.github.com>
2021-09-17 14:51:26 +0530

    Updates

---
+++ precache-manifest.dd143c5b0f63550c260d0d2036134809.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12c3b46d9b7ff085747c3a81bf6016c1",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "6536d4610e2e11614daf",
+    "url": "/react-gh-pages/static/css/2.486ebae0.chunk.css"
+  },
+  {
+    "revision": "4903e991d57d00fb56d4",
+    "url": "/react-gh-pages/static/css/main.e946fd69.chunk.css"
+  },
+  {
+    "revision": "6536d4610e2e11614daf",
+    "url": "/react-gh-pages/static/js/2.c6cdf57f.chunk.js"
+  },
+  {
+    "revision": "ccbf80019a0ad711a976987cc3ae88a1",
+    "url": "/react-gh-pages/static/js/2.c6cdf57f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4903e991d57d00fb56d4",
+    "url": "/react-gh-pages/static/js/main.6b1cd79e.chunk.js"
+  },
+  {
+    "revision": "1717f3cbd4ce90305bd8",
+    "url": "/react-gh-pages/static/js/runtime-main.e5a7967e.js"
+  }
+]);
